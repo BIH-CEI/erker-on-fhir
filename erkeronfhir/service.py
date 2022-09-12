634f88b79eb00f4535a353a56551a4d5e6e49aa4
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-12 14:10:27 +0200

    use auto-generated compability statement from base service

--- erkeronfhir/service.py
+++ erkeronfhir/service.py
@@ -2,11 +2,9 @@ from datetime import date
 from pathlib import Path
 from typing import List
 
-import fhir.resources
 from fasadeonfhir.config import config
 from fasadeonfhir.redcap.provider import RedcapProvider
 from fasadeonfhir.service import Service
-from fhir.resources.capabilitystatement import CapabilityStatement
 
 from erkeronfhir.capability import rest_capabilities
 
@@ -32,28 +30,3 @@ class ErkerService(Service):
     def _get_resources(self, resource_filter: List[str] = None):
         records = self.provider.get_records()
         return self.mapper.create_from_list(records, resource_filter=resource_filter)
-
-    def get_capability(self):
-        definition = {
-            "software": {"name": self.__app_name__},
-            "fhirVersion": fhir.resources.__fhir_version__,
-            "status": "active",
-            "kind": "capability",
-            "date": self.__capabilities_date__.isoformat(),
-            "format": ["json"],
-            "rest": [
-                {
-                    "mode": "server",
-                },
-            ],
-        }
-
-        if self.__capabilities_rest__:
-            definition["rest"][0]["resource"] = []
-            resources = definition["rest"][0]["resource"]
-
-            for resource, capability in self.__capabilities_rest__.items():
-                interactions = [{"code": item} for item in capability]
-                resources.append({"type": resource, "interaction": interactions})
-
-        return CapabilityStatement(**definition)
